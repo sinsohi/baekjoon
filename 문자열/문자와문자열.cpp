8f28ab3aac3504000c8f611406fc89800771f685
Sinsohi <sohi8451@naver.com>
2024-05-14 02:04:38 +0900

    백준 27866번 - 문자와 문자열

---
+++ 문자열/문자와문자열.cpp
@@ -0,0 +1,13 @@
+// 백준 27866번 - 문자와 문자열
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    int idx;
+    cin >> s >> idx;
+    cout << s[idx-1];
+    return 0;
+}   
---
+++ 문자열/문자와문자열.py
@@ -0,0 +1,5 @@
+# 백준 27866번 - 문자와 문자열
+
+Str = input()
+idx = int(input())
+print(Str[idx-1])
